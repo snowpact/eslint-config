50a04753fae73904e279de272fd6db21845649a3
Murat Avcı <murat.avci95@gmail.com>
2021-10-27 16:45:33 +0200

    Remove new babel config

--- react.js
+++ react.js
@@ -19,13 +19,7 @@ module.exports = {
     },
     version: 'detect'
   },
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ["@babel/preset-react"]
-    },
-  },
+  parser: 'babel-eslint',
   extends: [
     'standard',
     'standard-react',
